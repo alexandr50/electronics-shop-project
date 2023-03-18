e2d77bfc0d2dcf1d031f9b177e22024d552eea5a
alexandr50 <dmitrymaslov2016@mail.ru>
2023-03-18 12:26:10 +0300

    Добавил property number_of_sim и метод проверки на количество сим

--- src/phone.py
+++ src/phone.py
@@ -12,7 +12,26 @@ class Phone(Item):
     """
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.check_number_of_sim(number_of_sim)
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        '''Сеттер атрибута number_of_sim'''
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        '''Геттер атрибута number_of_sim'''
+        self.check_number_of_sim(value)
+        self.__number_of_sim = value
+
+    @staticmethod
+    def check_number_of_sim(value):
+        '''Проверка количества сим на меньше 1'''
+        if value <= 0:
+            raise ValueError('Количество сим должно быть больше 0')
+        return value
 
 
     def __repr__(self):
@@ -31,6 +50,5 @@ class Phone(Item):
         if type(other) not in (Item, Phone):
             raise TypeError('arguments must be class Item or Phone')
         return self.quantity + other.quantity
-
 
 
